54f9b38536c009d8cf0fe5bed421cc597706928b
VasaOfficial <VasaPofficial@gmail.com>
2022-12-27 15:03:33 +0100

    add function for getting data from api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import './style.css';
+
+// async function for getting weather data 
+async function getWeatherData(location) {
+  const apiKey = '8007d782d1fa9ea01ab64176725f4eb4';
+  const api = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`;
+
+  try {
+    const response = await fetch(api);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getWeatherData('London, ENG')
+
