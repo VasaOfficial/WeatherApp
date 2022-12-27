8b5bfa105ce7f1d50495e1054287740274c7e01f
VasaOfficial <VasaPofficial@gmail.com>
2022-12-27 15:43:28 +0100

    add function that process data

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './style.css';
 
-// async function for getting weather data 
+// getting weather data 
 async function getWeatherData(location) {
   const apiKey = '8007d782d1fa9ea01ab64176725f4eb4';
   const api = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`;
@@ -8,11 +8,35 @@ async function getWeatherData(location) {
   try {
     const response = await fetch(api);
     const data = await response.json();
-    console.log(data);
+    return data;
   } catch (error) {
     console.log(error);
   }
 }
 
-getWeatherData('London, ENG')
+// proccesing json data 
+function processWeatherData(data) {
+  const main = data.main;
+  const place = data.name;
+  const temp = main.temp;
+  const humidity = main.humidity;
+  const wind = data.wind;
+
+  return {
+    main,
+    place,
+    humidity,
+    temp,
+    wind
+  };
+}
+
+async function getProcessedWeatherData(location) {
+  const data = await getWeatherData(location);
+  return processWeatherData(data);
+}
+
+getProcessedWeatherData("New York, US").then(weatherData => {
+  console.log(weatherData);
+});
 
