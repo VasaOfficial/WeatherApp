373913d22f98a0b4b580d836d6b728cbcb0cca15
VasaOfficial <VasaPofficial@gmail.com>
2022-12-28 20:22:11 +0100

    add function for taking user input and displaying it

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import './style.css';
 // getting weather data 
 async function getWeatherData(location) {
   const apiKey = '8007d782d1fa9ea01ab64176725f4eb4';
-  const api = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`;
+  const api = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric`;
 
   try {
     const response = await fetch(api);
@@ -36,7 +36,38 @@ async function getProcessedWeatherData(location) {
   return processWeatherData(data);
 }
 
-getProcessedWeatherData("New York, US").then(weatherData => {
-  console.log(weatherData);
-});
+// get submit from user
+const form = document.querySelector('.form')
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const city = form.elements.city.value;
+  const country = form.elements.country.value;
+  const location = `${city}, ${country}`;
+  getProcessedWeatherData(location).then(weatherData => {
+    const placeElement = document.getElementById('place');
+  const tempElement = document.getElementById('temp');
+  const humidityElement = document.getElementById('humidity');
+  const windElement = document.getElementById('wind');
+
+  placeElement.textContent = weatherData.place;
+  tempElement.textContent = weatherData.temp + 'C';
+  humidityElement.textContent = weatherData.humidity + '%';
+  windElement.textContent = weatherData.wind.speed + 'km/h';
+  })
+})
+
+// clear displayed info 
+const resetBtn = document.querySelector('.reset')
+resetBtn.addEventListener('click', () => {
+  const placeElement = document.getElementById('place');
+  const tempElement = document.getElementById('temp');
+  const humidityElement = document.getElementById('humidity');
+  const windElement = document.getElementById('wind');
+
+  placeElement.textContent = '';
+  tempElement.textContent = '';
+  humidityElement.textContent = '';
+  windElement.textContent = '';
+})
+
 
